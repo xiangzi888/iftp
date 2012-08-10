68936d0c80956232c9a357a59ddc660a8d157bb6
xiangzi888 <xiangzi888888@gmail.com>
2012-08-11 01:09:17 +0800

    初始化，添加了一些空白文件

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/11/2012 01:02:11 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  xiangzi888 (), xiangzi888888@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
