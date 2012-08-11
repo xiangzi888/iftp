24657ba477a3c7dabb9efbe213992de5ad79025a
xiangzi888 <xiangzi888888@gmail.com>
2012-08-11 10:47:40 +0800

    更改了文件组织结构

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  main.c
  *
- *    Description:  
+ *    Description:  程序入口
  *
  *        Version:  1.0
  *        Created:  08/11/2012 01:02:11 AM
@@ -15,5 +15,22 @@
  *
  * =====================================================================================
  */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+#include <arpa/telnet.h>
+#include <unistd.h>
+#include <netdb.h>
+
+
+#include "ftpvar.h"
+
+
+
+
 
